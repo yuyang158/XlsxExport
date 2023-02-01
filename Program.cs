f088a13adde77195c437ec3d6214453375c58882
yuyang158 <yuyang_158@hotmail.com>
2023-02-01 17:01:26 +0800

    重复ID检查

--- Program.cs
+++ Program.cs
@@ -10,20 +10,20 @@ using System.Reflection;
 namespace ExcelExport {
 	class Program {
 		private static bool ArrayTypeCheck(IRow nameRow, string nameToCheck, int ignoreColumnIndex) {
-			if (nameToCheck.StartsWith("ignore")) {
+			if( nameToCheck.StartsWith("ignore") ) {
 				return false;
 			}
-			for (int i = 0; i < nameRow.LastCellNum; i++) {
-				if (ignoreColumnIndex == i) {
+			for( int i = 0; i < nameRow.LastCellNum; i++ ) {
+				if( ignoreColumnIndex == i ) {
 					continue;
 				}
 
 				var cell = nameRow.GetCell(i);
-				if (cell == null || cell.CellType == CellType.Blank) {
+				if( cell == null || cell.CellType == CellType.Blank ) {
 					continue;
 				}
 
-				if (cell.StringCellValue == nameToCheck) {
+				if( cell.StringCellValue == nameToCheck ) {
 					return true;
 				}
 			}
@@ -34,11 +34,11 @@ namespace ExcelExport {
 		private static bool ClassTypeCheck(string nameToCheck, out string key, out string field) {
 			key = string.Empty;
 			field = string.Empty;
-			if (nameToCheck.StartsWith("ignore")) {
+			if( nameToCheck.StartsWith("ignore") ) {
 				return false;
 			}
 
-			if (nameToCheck.Contains(".")) {
+			if( nameToCheck.Contains(".") ) {
 				var nameParts = nameToCheck.Split('.');
 				key = nameParts[0];
 				field = nameParts[1];
@@ -49,7 +49,7 @@ namespace ExcelExport {
 
 		static void Main() {
 			var excelFiles = JArray.Parse(File.ReadAllText("./Config/Export.json"));
-			foreach (string excelFilePath in excelFiles) {
+			foreach( string excelFilePath in excelFiles ) {
 				ConvertExcelFile(excelFilePath);
 			}
 
@@ -58,7 +58,7 @@ namespace ExcelExport {
 			Console.WriteLine("Start server end export");
 			m_exportScope = "s";
 			m_serverFileAppend = "_Server";
-			foreach (string excelFilePath in excelFiles) {
+			foreach( string excelFilePath in excelFiles ) {
 				ConvertExcelFile(excelFilePath);
 			}
 
@@ -67,8 +67,8 @@ namespace ExcelExport {
 				.SelectMany(s => s.GetTypes())
 				.Where(p => type.IsAssignableFrom(p));
 
-			foreach (var t in types) {
-				if(t.IsInterface) {
+			foreach( var t in types ) {
+				if( t.IsInterface ) {
 					continue;
 				}
 				var checkInstance = Activator.CreateInstance(t) as IPostExportCheck;
@@ -90,11 +90,11 @@ namespace ExcelExport {
 			Console.ForegroundColor = ConsoleColor.White;
 			Console.WriteLine($"Open xlsx file: {excelFilePath}.");
 			try {
-				using (var stream = new FileStream($"../{excelFilePath}", FileMode.Open)) {
+				using( var stream = new FileStream($"../{excelFilePath}", FileMode.Open) ) {
 					XSSFWorkbook workbook = new XSSFWorkbook(stream);
-					for (int i = 0; i < workbook.NumberOfSheets; i++) {
+					for( int i = 0; i < workbook.NumberOfSheets; i++ ) {
 						var sheetName = workbook.GetSheetName(i);
-						if (sheetName.StartsWith("ignore", StringComparison.InvariantCultureIgnoreCase)) {
+						if( sheetName.StartsWith("ignore", StringComparison.InvariantCultureIgnoreCase) ) {
 							continue;
 						}
 						Console.WriteLine($"Export sheet : {sheetName}");
@@ -105,14 +105,14 @@ namespace ExcelExport {
 						var clientServerScopeRow = sheet.GetRow(3);
 
 						List<int> exportColumnIndeies = new List<int>();
-						for (int columnIndex = 0; columnIndex < nameRow.LastCellNum; columnIndex++) {
+						for( int columnIndex = 0; columnIndex < nameRow.LastCellNum; columnIndex++ ) {
 							var cell = nameRow.GetCell(columnIndex);
-							if (cell == null || cell.CellType == CellType.Blank) {
+							if( cell == null || cell.CellType == CellType.Blank ) {
 								continue;
 							}
 
-							if (cell.CellType == CellType.String) {
-								if (cell.StringCellValue.StartsWith("ignore", StringComparison.InvariantCultureIgnoreCase)) {
+							if( cell.CellType == CellType.String ) {
+								if( cell.StringCellValue.StartsWith("ignore", StringComparison.InvariantCultureIgnoreCase) ) {
 									continue;
 								}
 
@@ -121,16 +121,16 @@ namespace ExcelExport {
 						}
 
 						BuildStructure(nameRow, typeRow, clientServerScopeRow, exportColumnIndeies, out var structures, out var translates);
-						if(structures.Count == 0) {
+						if( structures.Count == 0 ) {
 							continue;
 						}
 						string exportFilePath = $"{m_exportDirectory}/{sheetName}{m_serverFileAppend}.tsv";
 						Console.WriteLine($"Export {sheetName} to {exportFilePath} contains row : {sheet.LastRowNum - 2}");
-						using (var fileStream = new FileStream(exportFilePath, FileMode.Create))
-						using (var writer = new StreamWriter(fileStream)) {
+						using( var fileStream = new FileStream(exportFilePath, FileMode.Create) )
+						using( var writer = new StreamWriter(fileStream) ) {
 							string[] names = new string[structures.Count];
 							string[] types = new string[structures.Count];
-							for (int structureIndex = 0; structureIndex < structures.Count; structureIndex++) {
+							for( int structureIndex = 0; structureIndex < structures.Count; structureIndex++ ) {
 								var structure = structures[structureIndex];
 								names[structureIndex] = structure.ColumnName;
 								types[structureIndex] = structure.ColumnType;
@@ -141,22 +141,30 @@ namespace ExcelExport {
 
 							string[] values = new string[structures.Count];
 							int ignoreHeaderRowCount = 0;
-							foreach (IRow row in sheet) {
-								if (ignoreHeaderRowCount < 4) {
+							HashSet<string> idDuplicateCheck = new HashSet<string>();
+							foreach( IRow row in sheet ) {
+								if( ignoreHeaderRowCount < 4 ) {
 									ignoreHeaderRowCount++;
 									continue;
 								}
-								if (row == null || row.Cells.Count == 0) {
+								if( row == null || row.Cells.Count == 0 ) {
 									continue;
 								}
 								currentExportRow = row.RowNum;
 								var id = structures[0].ConvertValue(row);
-								if (string.IsNullOrEmpty(id)) {
+								if( string.IsNullOrEmpty(id) ) {
 									continue;
 								}
-								for (int structureIndex = 0; structureIndex < structures.Count; structureIndex++) {
+
+								if( idDuplicateCheck.Contains(id) ) {
+									Console.ForegroundColor = ConsoleColor.Red;
+									Console.WriteLine($"Id duplicate row ：{row.RowNum}, id : {id}");
+									return;
+								}
+
+								for( int structureIndex = 0; structureIndex < structures.Count; structureIndex++ ) {
 									var structure = structures[structureIndex];
-									if (structure is TranslateTypeStructure) {
+									if( structure is TranslateTypeStructure ) {
 										var translateStructure = structure as TranslateTypeStructure;
 										translateStructure.ID = id;
 									}
@@ -167,24 +175,24 @@ namespace ExcelExport {
 							}
 						}
 
-						if (translates.Count == 0) {
+						if( translates.Count == 0 ) {
 							continue;
 						}
 
 						string exportI18nFilePath = $"../Export/{sheetName}{m_serverFileAppend}_i18n.tsv";
-						using (var fileStream = new FileStream(exportI18nFilePath, FileMode.Create))
-						using (var writer = new StreamWriter(fileStream)) {
+						using( var fileStream = new FileStream(exportI18nFilePath, FileMode.Create) )
+						using( var writer = new StreamWriter(fileStream) ) {
 							writer.WriteLine("id\tcn");
 							writer.WriteLine("string\tstring");
 
-							foreach (var t in translates) {
+							foreach( var t in translates ) {
 								writer.WriteLine($"{t.Key}\t{t.Value}");
 							}
 						}
 					}
 				}
 			}
-			catch (Exception e) {
+			catch( Exception e ) {
 				Console.ForegroundColor = ConsoleColor.Red;
 				Console.WriteLine($"Error occurred while export row : {currentExportRow}, {exportColumnName}");
 				Console.WriteLine(e.Message);
@@ -196,17 +204,17 @@ namespace ExcelExport {
 			structures = new List<ITypeStructure>();
 			translates = new Dictionary<string, string>();
 			Console.WriteLine($"Export name rows : {string.Join(",", exportColumnIndeies)}");
-			foreach (var index in exportColumnIndeies) {
+			foreach( var index in exportColumnIndeies ) {
 				var type = typeRow.GetCell(index);
 				var name = nameRow.GetCell(index).StringCellValue;
 				var clientServerScope = clientServerScopeRow.GetCell(index).StringCellValue;
-				if(!clientServerScope.Contains(m_exportScope)) {
+				if( !clientServerScope.Contains(m_exportScope) ) {
 					continue;
 				}
 
 				ITypeStructure simpleType;
 				var classType = false;
-				if (ClassTypeCheck(name, out var key, out var field)) {
+				if( ClassTypeCheck(name, out var key, out var field) ) {
 					simpleType = TranslateSimpleType(translates, index, type, field);
 					classType = true;
 				}
@@ -216,17 +224,17 @@ namespace ExcelExport {
 				}
 
 				// array type check
-				if (complexTypes.TryGetValue(key, out var complexType)) {
+				if( complexTypes.TryGetValue(key, out var complexType) ) {
 					complexType.AppendColumnIndex(simpleType);
 				}
-				else if (classType) {
+				else if( classType ) {
 					complexType = new ClassTypeStructure(key);
 					complexType.AppendColumnIndex(simpleType);
 					complexTypes.Add(key, complexType);
 					structures.Add(complexType);
 				}
-				else if (ArrayTypeCheck(nameRow, name, index)) {
-					if (type.StringCellValue == "link") {
+				else if( ArrayTypeCheck(nameRow, name, index) ) {
+					if( type.StringCellValue == "link" ) {
 						complexType = new JsonArrayTypeStructure(name);
 					}
 					else {
@@ -244,28 +252,28 @@ namespace ExcelExport {
 		}
 
 		private static ITypeStructure TranslateSimpleType(Dictionary<string, string> translates, int columnIndex, ICell typeCell, string columnName) {
-			if (typeCell.StringCellValue == "string") {
+			if( typeCell.StringCellValue == "string" ) {
 				return new StringTypeStructure(columnIndex, columnName);
 			}
-			else if (typeCell.StringCellValue == "color") {
+			else if( typeCell.StringCellValue == "color" ) {
 				return new ColorTypeStructure(columnIndex, columnName);
 			}
 			else if( typeCell.StringCellValue == "json" ) {
 				return new JsonTypeStructure(columnIndex, columnName);
 			}
-			else if (typeCell.StringCellValue == "int" || typeCell.StringCellValue == "number") {
+			else if( typeCell.StringCellValue == "int" || typeCell.StringCellValue == "number" ) {
 				return new NumberTypeStructure(columnIndex, columnName);
 			}
-			else if (typeCell.StringCellValue == "bool" || typeCell.StringCellValue == "boolean") {
+			else if( typeCell.StringCellValue == "bool" || typeCell.StringCellValue == "boolean" ) {
 				return new BooleanTypeStructure(columnIndex, columnName);
 			}
-			else if (typeCell.StringCellValue == "translate") {
+			else if( typeCell.StringCellValue == "translate" ) {
 				return new TranslateTypeStructure(columnIndex, columnName, translates);
 			}
-			else if (typeCell.StringCellValue == "link") {
+			else if( typeCell.StringCellValue == "link" ) {
 				return new LinkTypeStructure(columnIndex, columnName);
 			}
-			else if (typeCell.StringCellValue == "links") {
+			else if( typeCell.StringCellValue == "links" ) {
 				return new LinksTypeStructure(columnIndex, columnName);
 			}
 			else {
